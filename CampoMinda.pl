7375af73dd0702d8563c95976ed631c3d9f0ec78
Gustavo <gusrmonteiro@live.com>
2017-05-14 17:45:30 -0300

    Problema 2, feito!
    Muitas alterações foram feitas, a principal é que o jogo precisa começar
    com "start_game" e assim gerar o arquivo " Board.txt" com o tabuleiro.
    Então, cada jogada o programa avalia esse arquivo e gera dinamicamente o
    arquivo "Ambiente.txt".

--- CampoMinda.pl
+++ CampoMinda.pl
@@ -1,32 +1,40 @@
-main:-
-	open('Entrada.txt',read,Input),
-	read(Input,Bord_size),
-	tabuleiro(Size) = Bord_size,
-	read_mines(List_minas,Input),
-	close(Input),
+start_game:- % Programa 1
+	open('Mina.txt',read,Mina_file),
+	read(Mina_file,Board_size),
+	tabuleiro(Size) = Board_size,
+	read_mines(List_minas,Mina_file),
+	close(Mina_file),
 
 	search_board(Size,List_minas,Size,List_board),
 
-	open('Saida.txt',write,Output),
+	open('Board.txt',write,Board_file),
+	write_values(List_board,Board_file),
+	close(Board_file),
+	open('Ambiente.txt',write,Temp),close(Temp), % zera o ambiente
+	!.
 
-	print_output(List_board,Output),
-
-	close(Output),!.
-
-/*Le as minas de um arquivo*/
-read_mines([X|L],File):- 
+/*Le uma lista de um arquivo*/
+read_list([X|L],File):- 
 	read(File,X),
 	not(X = end_of_file),
-	read_mines(L,File),!.
+	read_list(L,File),
+	!.
+read_list([],_):-!.
+read_mines(List_minas,File):- read_list(List_minas,File).
+read_values(List_values,File):- read_list(List_values,File).
 
-read_mines([],_):-!.
 
 /*Imprime uma lista em um arquivo com '.' e quebra de linha*/
-print_output([X|L],File):-
+print_list([X|L],File):-
 	write(File,X), write(File,'.'), nl(File),
-	print_output(L,File),!.
+	print_list(L,File),
+	!.
+print_list([],_):-!.
+write_values(L,File):- print_list(L,File).
 
-print_output([],_):-!.
+write_screen(X):- write(X),write('.'),nl.
+write_list_screen([]).
+write_list_screen([X|L]):- write_screen(X), write_list_screen(L).
 
 /*Percorre as linhas da matriz*/
 search_board(1,List_minas,Size,List_board):-
@@ -55,86 +63,81 @@ get_element_Valor(Row,Col,_,List_minas,[]):- member(mina(Row,Col),List_minas),!.
 get_element_Valor(Row,Col,Size,List_minas,[valor(Row,Col,K)]):- mine_neighbor(valor(Row,Col,K),List_minas,Size),!.
 
 /*Descobre valor K de bonbas ao redor de um campo*/
-mine_neighbor(valor(Row,Col,K),List_in,Size):-
+mine_neighbor(valor(Row,Col,K),List_minas,Size):-
 		Row =< Size, Row >= 1,
 		Col =< Size, Col >= 1,
 		Aux is 0,
-		there_is_mine(Row,Col,1,List_in,Mina1),
-		Aux1 is Aux + Mina1,
-		there_is_mine(Row,Col,2,List_in,Mina2),
-		Aux2 is Aux1 + Mina2,
-		there_is_mine(Row,Col,3,List_in,Mina3),
-		Aux3 is Aux2 + Mina3,
-		there_is_mine(Row,Col,4,List_in,Mina4),
-		Aux4 is Aux3 + Mina4,
-		there_is_mine(Row,Col,5,List_in,Mina5),
-		Aux5 is Aux4 + Mina5,
-		there_is_mine(Row,Col,6,List_in,Mina6),
-		Aux6 is Aux5 + Mina6,
-		there_is_mine(Row,Col,7,List_in,Mina7),
-		Aux7 is Aux6 + Mina7,
-		there_is_mine(Row,Col,8,List_in,Mina8),
-		Aux8 is Aux7 + Mina8,
+		there_is_mine(Row,Col,1,List_minas,Mina1), Aux1 is Aux  + Mina1,
+		there_is_mine(Row,Col,2,List_minas,Mina2), Aux2 is Aux1 + Mina2,
+		there_is_mine(Row,Col,3,List_minas,Mina3), Aux3 is Aux2 + Mina3,
+		there_is_mine(Row,Col,4,List_minas,Mina4), Aux4 is Aux3 + Mina4,
+		there_is_mine(Row,Col,5,List_minas,Mina5), Aux5 is Aux4 + Mina5,
+		there_is_mine(Row,Col,6,List_minas,Mina6), Aux6 is Aux5 + Mina6,
+		there_is_mine(Row,Col,7,List_minas,Mina7), Aux7 is Aux6 + Mina7,
+		there_is_mine(Row,Col,8,List_minas,Mina8), Aux8 is Aux7 + Mina8,
 		K = Aux8
 		.
 
 /*there_is_mine: diz se tem mina em um determinado vizinho*/
-there_is_mine(Row,Col,1,List_in,1):-
+there_is_mine(Row,Col,1,List_minas,1):-
 	Y is Col - 1,
 	X is Row + 1,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,2,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,2,List_minas,1):-
 	Y is Col,
 	X is Row + 1,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,3,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,3,List_minas,1):-
 	Y is Col + 1,
 	X is Row + 1,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,4,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,4,List_minas,1):-
 	Y is Col + 1,
 	X is Row,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,5,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,5,List_minas,1):-
 	Y is Col + 1,
 	X is Row - 1,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,6,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,6,List_minas,1):-
 	Y is Col,
 	X is Row - 1,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,7,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,7,List_minas,1):-
 	Y is Col - 1,
 	X is Row - 1,
-	member(mina(X,Y),List_in),!.
-
-there_is_mine(Row,Col,8,List_in,1):-
+	member(mina(X,Y),List_minas),
+	!.
+there_is_mine(Row,Col,8,List_minas,1):-
 	Y is Col - 1,
 	X is Row,
-	member(mina(X,Y),List_in),!.
-
+	member(mina(X,Y),List_minas),
+	!.
 there_is_mine(_,_,_,_,0).
 
 
+get_element_from_List_board(Row,Col,[X|_],X):- X = valor(Row,Col,_),!.
+get_element_from_List_board(Row,Col,[_|List_board],X):- get_element_from_List_board(Row,Col,List_board,X).
 
 /*Gera os campos que serão abertos apos uma jogada*/
-gera_valores(Row,Col,Size,List_minas,[]):- %campo fora do limite
-	not(mine_neighbor(valor(Row,Col,_),List_minas,Size)),
+gera_valores(Row,Col,_,_,List_minas,['jogo encerrado']):- % campo com mina 
+	member(mina(Row,Col),List_minas), /*Falta implementar a ação de limbar o tabuleiro!*/
 	!.
-
-gera_valores(Row,Col,Size,List_minas,[valor(Row,Col,K)]):- %campo com K>0 minas vizinhas
-	mine_neighbor(valor(Row,Col,K),List_minas,Size),
-	K > 0,
+gera_valores(Row,Col,Size,_,_,[]):- % campo fora do limite do tabuleiro
+	( Row > Size ; Row < 1 ; Col > Size ; Col < 1) ,
+	write('Campo fora do tabuleiro'),
 	!.
-
-gera_valores(Row,Col,Size,List_minas,[valor(Row,Col,K)|L]):- %campo com 0 minas vizinhas
-	mine_neighbor(valor(Row,Col,K),List_minas,Size),
+gera_valores(Row,Col,_,List_board,_,[valor(Row,Col,K)]):- % campo com K>0 minas vizinhas
+	get_element_from_List_board(Row,Col,List_board,valor(Row,Col,K)), K > 0,
+	!.
+gera_valores(Row,Col,Size,List_board,_,[valor(Row,Col,K)|L]):- % campo com 0 minas vizinhas
+	get_element_from_List_board(Row,Col,List_board,valor(Row,Col,K)),
 
 	R1 is Row + 1,
 	C1 is Col - 1,
@@ -160,29 +163,15 @@ gera_valores(Row,Col,Size,List_minas,[valor(Row,Col,K)|L]):- %campo com 0 minas 
 	R8 is Row,
 	C8 is Col - 1,
 
-	Ambiente1 is [valor(Row,Col,K)],
-	gera_valores(R1,C1,Size,List_minas,Ambiente1,L1),
-
-	add_filds_zeros(Ambiente1,L1,Ambiente2), 
-	gera_valores(R2,C2,Size,List_minas,Ambiente2,L2),
-
-	add_filds_zeros(Ambiente2,L2,Ambiente3), 
-	gera_valores(R3,C3,Size,List_minas,Ambiente3,L3),
-
-	add_filds_zeros(Ambiente3,L3,Ambiente4),
-	gera_valores(R4,C4,Size,List_minas,Ambiente4,L4),
-
-	add_filds_zeros(Ambiente4,L4,Ambiente5), 
-	gera_valores(R5,C5,Size,List_minas,Ambiente5,L5),
-
-	add_filds_zeros(Ambiente5,L5,Ambiente6), 
-	gera_valores(R6,C6,Size,List_minas,Ambiente6,L6),
-
-	add_filds_zeros(Ambiente6,L6,Ambiente7), 
-	gera_valores(R7,C7,Size,List_minas,Ambiente7,L7),
-
-	add_filds_zeros(Ambiente7,L7,Ambiente8), 
-	gera_valores(R8,C8,Size,List_minas,Ambiente8,L8),
+	Ambiente1 = [valor(Row,Col,K)],
+	gera_valores(R1,C1,Size,List_board,_,Ambiente1,L1), add_fields_zeros(Ambiente1,L1,Ambiente2), 
+	gera_valores(R2,C2,Size,List_board,_,Ambiente2,L2), add_fields_zeros(Ambiente2,L2,Ambiente3), 
+	gera_valores(R3,C3,Size,List_board,_,Ambiente3,L3), add_fields_zeros(Ambiente3,L3,Ambiente4),
+	gera_valores(R4,C4,Size,List_board,_,Ambiente4,L4), add_fields_zeros(Ambiente4,L4,Ambiente5), 
+	gera_valores(R5,C5,Size,List_board,_,Ambiente5,L5), add_fields_zeros(Ambiente5,L5,Ambiente6), 
+	gera_valores(R6,C6,Size,List_board,_,Ambiente6,L6), add_fields_zeros(Ambiente6,L6,Ambiente7), 
+	gera_valores(R7,C7,Size,List_board,_,Ambiente7,L7), add_fields_zeros(Ambiente7,L7,Ambiente8), 
+	gera_valores(R8,C8,Size,List_board,_,Ambiente8,L8),
 
 	append(L1,L2,Aux1),
 	append(Aux1,L3,Aux2),
@@ -191,26 +180,24 @@ gera_valores(Row,Col,Size,List_minas,[valor(Row,Col,K)|L]):- %campo com 0 minas 
 	append(Aux4,L6,Aux5),
 	append(Aux5,L7,Aux6),
 	append(Aux6,L8,Aux7),
-	limpar_list(Aux7,L),
+
+	retira_elemento(valor(Row,Col,K),Aux7,Aux8),
+	limpar_list(Aux8,L),
 	!.
 
-/*gera_valores/6 faz o mesmo que gera_valores/5. Existe para resolver loop infinito*/
-gera_valores(Row,Col,Size,List_minas,_,[]):-
-	not(mine_neighbor(valor(Row,Col,_),List_minas,Size)),
+/*gera_valores/7 faz o mesmo que gera_valores/6. Existe para resolver loop infinito*/
+gera_valores(Row,Col,Size,_,_,_,[]):- % campo fora do limite do tabuleiro
+	( Row > Size ; Row < 1 ; Col > Size ; Col < 1) ,
 	!.
-
-gera_valores(Row,Col,Size,List_minas,_,[valor(Row,Col,K)]):-
-	mine_neighbor(valor(Row,Col,K),List_minas,Size),
-	K > 0,
+gera_valores(Row,Col,_,List_board,_,_,[valor(Row,Col,K)]):- % campo com K>0 minas vizinhas
+	get_element_from_List_board(Row,Col,List_board,valor(Row,Col,K)), K > 0,
 	!.
-
-gera_valores(Row,Col,Size,List_minas,Ambiente,[valor(Row,Col,K)]):-
-	mine_neighbor(valor(Row,Col,K),List_minas,Size),
+gera_valores(Row,Col,_,List_board,_,Ambiente,[valor(Row,Col,K)]):- % campo com 0 minas vizinhas que já foi visitado.
+	get_element_from_List_board(Row,Col,List_board,valor(Row,Col,K)),
 	member(valor(Row,Col,K),Ambiente),
 	!.
-
-gera_valores(Row,Col,Size,List_minas,Ambiente,[valor(Row,Col,K)|L]):-
-	mine_neighbor(valor(Row,Col,K),List_minas,Size),
+gera_valores(Row,Col,Size,List_board,_,Ambiente,[valor(Row,Col,K)|L]):- % campo com 0 minas vizinhas novo
+	get_element_from_List_board(Row,Col,List_board,valor(Row,Col,K)),
 
 	R1 is Row + 1,
 	C1 is Col - 1,
@@ -237,28 +224,14 @@ gera_valores(Row,Col,Size,List_minas,Ambiente,[valor(Row,Col,K)|L]):-
 	C8 is Col - 1,
 	
 	append(Ambiente,[valor(Row,Col,K)],Ambiente1),
-	gera_valores(R1,C1,Size,List_minas,Ambiente1,L1),
-
-	add_filds_zeros(Ambiente1,L1,Ambiente2), 
-	gera_valores(R2,C2,Size,List_minas,Ambiente2,L2),
-
-	add_filds_zeros(Ambiente2,L2,Ambiente3), 
-	gera_valores(R3,C3,Size,List_minas,Ambiente3,L3),
-
-	add_filds_zeros(Ambiente3,L3,Ambiente4),
-	gera_valores(R4,C4,Size,List_minas,Ambiente4,L4),
-
-	add_filds_zeros(Ambiente4,L4,Ambiente5), 
-	gera_valores(R5,C5,Size,List_minas,Ambiente5,L5),
-
-	add_filds_zeros(Ambiente5,L5,Ambiente6), 
-	gera_valores(R6,C6,Size,List_minas,Ambiente6,L6),
-
-	add_filds_zeros(Ambiente6,L6,Ambiente7), 
-	gera_valores(R7,C7,Size,List_minas,Ambiente7,L7),
-
-	add_filds_zeros(Ambiente7,L7,Ambiente8), 
-	gera_valores(R8,C8,Size,List_minas,Ambiente8,L8),
+	gera_valores(R1,C1,Size,List_board,List_minas,Ambiente1,L1), add_fields_zeros(Ambiente1,L1,Ambiente2), 
+	gera_valores(R2,C2,Size,List_board,List_minas,Ambiente2,L2), add_fields_zeros(Ambiente2,L2,Ambiente3), 
+	gera_valores(R3,C3,Size,List_board,List_minas,Ambiente3,L3), add_fields_zeros(Ambiente3,L3,Ambiente4),
+	gera_valores(R4,C4,Size,List_board,List_minas,Ambiente4,L4), add_fields_zeros(Ambiente4,L4,Ambiente5), 
+	gera_valores(R5,C5,Size,List_board,List_minas,Ambiente5,L5), add_fields_zeros(Ambiente5,L5,Ambiente6), 
+	gera_valores(R6,C6,Size,List_board,List_minas,Ambiente6,L6), add_fields_zeros(Ambiente6,L6,Ambiente7), 
+	gera_valores(R7,C7,Size,List_board,List_minas,Ambiente7,L7), add_fields_zeros(Ambiente7,L7,Ambiente8), 
+	gera_valores(R8,C8,Size,List_board,List_minas,Ambiente8,L8),
 
 	append(L1,L2,Aux1),
 	append(Aux1,L3,Aux2),
@@ -267,44 +240,72 @@ gera_valores(Row,Col,Size,List_minas,Ambiente,[valor(Row,Col,K)|L]):-
 	append(Aux4,L6,Aux5),
 	append(Aux5,L7,Aux6),
 	append(Aux6,L8,Aux7),
+
 	limpar_list(Aux7,L),
 	!.
 
 /*Adiciona na Lout apenas campos com 0 minas ao redor*/
-add_filds_zeros(L,[],L).
-add_filds_zeros(L,[X|Lin],Lout):- X = valor(_,_,0), add_filds_zeros(L,Lin,Lout).
-add_filds_zeros(L,[X|Lin],Lout):- member(X,L), add_filds_zeros(L,Lin,Lout).
-add_filds_zeros(L,[X|Lin],Lout):- member(X,Lin), add_filds_zeros(L,Lin,Lout).
-add_filds_zeros(L,[X|Lin],[X|Lout]):- X = valor(_,_,K), K > 0, add_filds_zeros(L,Lin,Lout).
+add_fields_zeros(L,[],L).
+add_fields_zeros(L,[X|Lin],Lout):- X = valor(_,_,K), K > 0, add_fields_zeros(L,Lin,Lout),!.
+add_fields_zeros(L,[X|Lin],Lout):- member(X,L), add_fields_zeros(L,Lin,Lout),!.
+add_fields_zeros(L,[X|Lin],Lout):- member(X,Lin), add_fields_zeros(L,Lin,Lout),!.
+add_fields_zeros(L,[X|Lin],[X|Lout]):- X = valor(_,_,0), add_fields_zeros(L,Lin,Lout),!.
 
 /*Tira elementos repetidos*/
 limpar_list([],[]).
 limpar_list([X|L],Lout):- member(X,L), limpar_list(L,Lout),!.
 limpar_list([X|L],[X|Lout]):- limpar_list(L,Lout),!.
 
+retira_elemento(_,[],[]).
+retira_elemento(X,[X|L],Lout):- retira_elemento(X,L,Lout),!.
+retira_elemento(X,[Y|L],[Y|Lout]):- retira_elemento(X,L,Lout),!.
 
 /*Cada Jogada*/
-posicao(Row,Col):-
+posicao(Row,Col):- % Programa 2 
 
-	open('Entrada.txt',read,Input),
-	read(Input,Bord_size),
-	tabuleiro(Size) = Bord_size,
-	read_mines(List_minas,Input),
-	close(Input),
+	open('Mina.txt',read,Mina_file),
+	read(Mina_file,Board_size),
+	tabuleiro(Size) = Board_size,
+	read_mines(List_minas,Mina_file),
+	close(Mina_file),
 
-	gera_valores(Row,Col,Size,List_minas,Novo_Ambiente),
-	/*
-	alex_func(Novo_Ambiente,Ambiente),
-	*/
+	open('Board.txt',read,Board_file),
+	read_values(List_board,Board_file),
+	close(Board_file),
 
-	open('Saida.txt',write,Output),
+	gera_valores(Row,Col,Size,List_board,List_minas,Novo_Ambiente),
+	imrpimr_ambiente(Novo_Ambiente,List_board),
 
-	print_output(Novo_Ambiente,Output),
+	!.
 
+imrpimr_ambiente(Novo_Ambiente,List_board):-
+  open('Ambiente.txt',read,Ambiente),
+  read_values(List_Ambiente,Ambiente),
+  close(Ambiente),
 
-	close(Output),!.
+  uniao_lista(Novo_Ambiente,List_Ambiente,Ambiente_final),
 
+  is_game_over(List_board,Ambiente_final),
 
+  open('Ambiente.txt',write,Output),
+  write_values(Ambiente_final,Output),
+  close(Output).
 
+uniao_lista([],L,L).
+uniao_lista([X|L1],L2,L3):-
+	member(X,L2),
+	uniao_lista(L1,L2,L3),
+	!.
+uniao_lista([X|L1],L2,[X|L3]):-
+    not(member(X,L2)),
+    write_screen(X),
+    uniao_lista(L1,L2,L3),
+    !.
+
+contains_list([],_).
+contains_list([X|L1],L2):- member(X,L2), contains_list(L1,L2).
+
+is_game_over(List_board,List_ambiente):- contains_list(List_board,List_ambiente), write('You win! Game Over!'), nl.
+is_game_over(_,_).
 
 
